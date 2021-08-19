8417452db3c221da8f04fa74b41054aff4327c15
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-08-19 18:08:45 -0500

    Add files via upload

---
+++ precache-manifest.68b93d06f347b2251544f5dee752e0f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430aac70cbff1a2577147ccab85386a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a0f8594924422b0ddbf",
+    "url": "/static/js/2.bec2cdb7.chunk.js"
+  },
+  {
+    "revision": "d5b445a81227876e9f4080bf0648eb92",
+    "url": "/static/js/2.bec2cdb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6dd0e7eedd46611d37",
+    "url": "/static/js/main.e000827b.chunk.js"
+  },
+  {
+    "revision": "a26cc4ea470caf745781",
+    "url": "/static/js/runtime-main.747b6b70.js"
+  }
+]);
